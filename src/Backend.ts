3e77cc38804be3822b3ddcbc3123cbe934cfced3
Louis Garavaglia <louis.garavaglia@gmail.com>
2021-04-01 19:19:44 -0700

    changed proces.variables

--- src/Backend.ts
+++ src/Backend.ts
@@ -14,7 +14,8 @@ axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 // const BASE_URL = "https://e4b45af1fadf.ngrok.io/";
 // const BASE_URL = "http://127.0.0.1:8000";
 // const BASE_URL = "https://acoustic-backend.herokuapp.com";
-const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://127.0.0.1:8000';
+// const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://127.0.0.1:8000';
+const BASE_URL = 'https://acoustic-backend.herokuapp.com';
 
 
 // interface TokenObj {
