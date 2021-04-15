fdd15f55c090f35a1e09419c39141ff577c9d49f
Louis Garavaglia <louis.garavaglia@gmail.com>
2021-04-15 11:20:28 -0700

    updated classNames for Navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -11,12 +11,12 @@ const Navbar: React.FC = () => {
   return (
     <>
       <div className='Navbar-Container-Left'>
-        <div className='Navbar-Search-Box'>
+        <div className='Navbar-Logo-Box'>
             <NavLink  className='Navbar-Logo' exact to='/'><span className='Navbar-Logo-Circle'>A.IO</span></NavLink>
         </div>
       </div>
       <div className='Navbar-Container-Right'>
-        <div className='Navbar-Browse-Box'>
+        <div className='Navbar-Links-Box'>
             <NavLink  className='Navbar-Login' exact to='/login'>LOGIN</NavLink>
         </div>
       </div>
