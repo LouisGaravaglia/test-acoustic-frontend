1c7d7029bb47ef835856bb2246b2709440a0cffd
Louis Garavaglia <louis.garavaglia@gmail.com>
2021-04-16 19:09:55 -0700

    styling for track names and track artists at a good place now

--- src/Components/PlaylistsContainer.tsx
+++ src/Components/PlaylistsContainer.tsx
@@ -26,18 +26,23 @@ function PlaylistsContainer(): JSX.Element {
     <div className='Playlists-Container'>
 
       <div className="Playlists-Header-Box">
-        <h1 className="Playlists-Selected-Title">{fakePlaylist.name}</h1>
+        <h1 className="Playlists-Selected-Title">{fakePlaylist.name.toUpperCase()}</h1>
       </div>
 
       <div className="Playlists-Selected-Content">
-        <div className='Playlists-Left-Box'>
+
+        <div className='Playlists-Artwork-Box'>
         </div>
 
-        <div className='Playlists-Right-Box'>
+        <div className='Playlists-Tracks-Container'>
             {tracks.map((value: any, index: any) => 
-              <div key={value.track.id}>
-                <p className='Playlists-Track-Name'>{value.track.name}</p>
-                <p className='Playlists-Artist-Name'>{value.track.artists[0].name}</p>
+              <div key={value.track.id} className='Playlists-Tracks-Box'>
+                {/* <div className="Playlists-Track"> */}
+                  <p className='Playlists-Track-Name'>{value.track.name}</p>
+                {/* </div> */}
+                {/* <div className="Playlists-Artist"> */}
+                  <p className='Playlists-Artist-Name'>{value.track.artists[0].name.toUpperCase()}</p>
+                {/* </div> */}
             </div>
             )}
         </div>
