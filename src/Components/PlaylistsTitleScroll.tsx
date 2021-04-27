c522c2f46d4bcaa14ac39db74ee06992a1f0d935
Louis Garavaglia <louis.garavaglia@gmail.com>
2021-04-27 13:37:26 -0700

    title scroll is working with the large type being centered by just using intersection observer. Though can only work if there are two large titles existing for a split second

--- src/Components/PlaylistsTitleScroll.tsx
+++ src/Components/PlaylistsTitleScroll.tsx
@@ -1,4 +1,5 @@
 import React, {useState, useRef, useEffect, useMemo, useCallback} from 'react';
+import useViewport from '../Hooks/useViewport';
 import useElementOnScreen from '../Hooks/useElementOnScreen.tsx';
 let playlists =  require('../fakeData/playlist.json');
 
@@ -7,8 +8,8 @@ interface Props {
     "id": string
     "name": string
   }
-  // index: number
-  // handleScroll: () => void
+  index: number
+  handleInViewport: (titleRef: any | null, index: number) => void
   // // boxRef: React.Ref<HTMLInputElement>
   // titleInsideBoundary: boolean
   // titleContainerRef: {
@@ -21,29 +22,87 @@ interface Props {
 //   isVisible: boolean
 // }
 
-function PlaylistsTitleScroll({playlist}: Props): JSX.Element {
+function PlaylistsTitleScroll({playlist, handleInViewport, index}: Props): JSX.Element {
   // const [isVisible, setIsVisible] = useState<boolean>(false);
-  const boxRef = useRef<any | null>(null);
-  // const entry = useElementOnScreen(boxRef, {
+  const titleRef = useRef<any | null>(null);
+  let leftSideOfTitleDiv: number = 0;
+  let rightSideOfTitleDiv: number = 0;
+  if (titleRef.current !== null) {
+    const {left, right} = titleRef.current.getBoundingClientRect();
+    leftSideOfTitleDiv = left;
+    rightSideOfTitleDiv = right;
+  }
+  const {viewportWidth}  = useViewport();
+  const viewportMidPoint = viewportWidth / 2;
+  // const entry = useElementOnScreen(titleRef, {
   //   // root: document.querySelector('.scrolling-wrapper'),
   //   rootMargin: "0px -400px",
   //   threshold: 0.9
   // });
   // const isVisible = !!entry?.isIntersecting;
-  const entry = useElementOnScreen(boxRef, {
+  const entry = useElementOnScreen(titleRef, {
     root: document.querySelector('.scrolling-wrapper'),
-    rootMargin: "0px -100px",
-    threshold: 0.9
+    rootMargin: "0px -300px",
+    threshold: 0.40
   });
-    const isVisible = !!entry?.isIntersecting;
+  const isVisible = !!entry?.isIntersecting;
+
+    // useEffect(() => {
+
+    //   handleInViewport(titleRef, index);
+    // }, [titleRef, enteredFrame])
+
+    // handleInViewport(titleRef);
+
+    // const top = titleRef.current.getBoundingClientRect().top;
+    // const enteredFrame = !!entry?.isIntersecting && (top > 143 && top < 144);
+  // titleRef.current = index;
+
+    // useEffect(() => {
+    //   function isInViewport() {
+    //     // if (!titleRef) return false;
+        
+    //     // const enteredFrame = !!entry?.isIntersecting && (leftSideOfTitleDiv < viewportMidPoint && rightSideOfTitleDiv > viewportMidPoint);
+    //     const touchingMidPoint = (leftSideOfTitleDiv < viewportMidPoint && rightSideOfTitleDiv > viewportMidPoint);
+
+    //     setIsVisible(touchingMidPoint)
+    //     // console.log(`touchingMidPoint ${index}`, enteredFrame);
+        
+    //     console.log(`leftSideOfTitleDiv ${index}`, leftSideOfTitleDiv);
+    
+    //     console.log('midpoint', viewportMidPoint);
+    //     console.log(`rightSideOfTitleDiv ${index}`, rightSideOfTitleDiv);
+        
+
+    //   }
+
+    //   isInViewport();
+    // }, [titleRef, setIsVisible, leftSideOfTitleDiv, rightSideOfTitleDiv])
 
 
-console.log("isVisible: ", isVisible);
+      // function isInViewport(offset = 0) {
+      //   console.log("hit");
+        
+      //   if (!titleRef) return false;
+      //   const {left, right} = titleRef.current.getBoundingClientRect();
+      //   // const enteredFrame = !!entry?.isIntersecting && (left < viewportMidPoint && right > viewportMidPoint);
+      //   const enteredFrame = (left < viewportMidPoint && right > viewportMidPoint);
+
+      //   setIsVisible(enteredFrame)
+      //   console.log(`index ${index}`, left);
+      //   console.log('midpoint', viewportMidPoint);
+        
+      //   if (enteredFrame) observeTitlesInView(index);
+      //   // return (top + offset) >= 0 && (top - offset) <= window.innerHeight;
+      // }
+
+
+// console.log("isVisible: ", isVisible);
 
 
 
   return (
-      <div className={isVisible ? 'Playlists-Featured-Title' : 'card'} ref={boxRef}><h2 >{playlist.name}</h2></div>
+      <div className={isVisible ? 'Playlists-Featured-Title' : 'card'} ref={titleRef} ><h2 >{playlist.name}</h2></div>
   );
 };
 
--- src/Components/PlaylistsTitleScrollContainer.tsx
+++ src/Components/PlaylistsTitleScrollContainer.tsx
@@ -1,16 +1,46 @@
 import React, {useState, useRef} from 'react';
 import PlaylistsTitleScroll from './PlaylistsTitleScroll';
+import useViewport from '../Hooks/useViewport';
 let playlists =  require('../fakeData/playlist.json');
 
 function PlaylistsTitleScrollContainer(): JSX.Element {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const {viewportWidth}  = useViewport();
 
+  function observeTitlesInView(index: number) {
+    console.log('index in view: ', index);
+    
+  }
+
+  function handleInViewport(titleRef: any | null, index: number) {
+    console.log("hit");
+    
+    if (titleRef.current === null) return;
+    const {left, right} = titleRef.current.getBoundingClientRect();
+
+    const viewportMidPoint = viewportWidth / 2;
+    // const enteredFrame = !!entry?.isIntersecting && (left < viewportMidPoint && right > viewportMidPoint);
+    const enteredFrame = (left < viewportMidPoint && right > viewportMidPoint);
+
+    setIsVisible(enteredFrame)
+    console.log(`enteredFrame ${index}`, enteredFrame);
+    
+    console.log(`LEFT ${index}`, left);
+
+    console.log('midpoint', viewportMidPoint);
+    console.log(`RIGHT ${index}`, right);
+    
+    // if (enteredFrame) observeTitlesInView(index);
+    // return (top + offset) >= 0 && (top - offset) <= window.innerHeight;
+  }
 
   return (
     // <div className="scrollingContainer">
 
-    <div className="scrolling-wrapper">
-
-      {playlists.map((playlist: any, index: any) => <PlaylistsTitleScroll key={index} playlist={playlist}/>)}
+    <div className="scrolling-wrapper" >
+      <div className='card' ><h2 ></h2></div>
+      {playlists.map((playlist: any, index: any) => <PlaylistsTitleScroll key={index} index={index} playlist={playlist} handleInViewport={handleInViewport}/>)}
+      <div className='card' ><h2 ></h2></div>
 
     </div>
     // </div>
