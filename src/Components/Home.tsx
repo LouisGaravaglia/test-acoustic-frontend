fc14ba0a615405ac6aef7b622f8137f3663e4326
Louis Garavaglia <louis.garavaglia@gmail.com>
2021-05-13 18:07:36 -0700

    playing with transition and opacity for background svg display on page load

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -30,26 +30,30 @@ const Home: React.FC = () => {
   }
 
   return (
-    <div className='Home-Container' style={{opacity: homeMounted ? 1 : 0}}>
-      <div className='Home-Header-Box'>
-        <h1>ACOUSTIGRAM.IO</h1>
-        <h5>A Digital Companion to keep you up to date with the latest music releases.</h5>
-      </div>
-      <div className="Home-Lower-Container">
-        <div className="Home-Button-Container">
-          <div onClick={redirectToChatty} className="Home-Button-Box">
-            <h1 className="Home-Button">Get Started</h1>
-            <IoIosArrowRoundForward color='#181718' className='Home-Register-Arrow'/>
+    <>
+    <section className='Home' style={{opacity: homeMounted ? 1 : 0}}>
+          </section>
+      <div className='Home-Container'>
+        <div className='Home-Header-Box'>
+          <h1>ACOUSTIGRAM.IO</h1>
+          <h5>A Digital Companion to keep you up to date with the latest music releases.</h5>
+        </div>
+        <div className="Home-Lower-Container">
+          <div className="Home-Button-Container">
+            <div onClick={redirectToChatty} className="Home-Button-Box">
+              <h1 className="Home-Button">Get Started</h1>
+              <IoIosArrowRoundForward color='#181718' className='Home-Register-Arrow'/>
+            </div>
+          </div>
+          <div className="Home-Login-Container">
+          <div onClick={redirectToLogin} className="Home-Login-Box">
+              <h1 className="Home-Login">Login</h1>
+              <IoIosArrowRoundForward color='#181718' className='Home-Login-Arrow'/>
+            </div>
           </div>
         </div>
-        <div className="Home-Login-Container">
-        <div onClick={redirectToLogin} className="Home-Login-Box">
-            <h1 className="Home-Login">Login</h1>
-            <IoIosArrowRoundForward color='#181718' className='Home-Login-Arrow'/>
-          </div>
-        </div>
       </div>
-    </div>
+</>
   );
 };
 
